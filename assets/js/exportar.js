9be17b9e603d4a937a50038bb9f4651b6c3b1fb2
Helerson Junior <helersonjunior@gmail.com>
2024-04-02 14:41:20 -0400

    atualizando

---
+++ assets/js/exportar.js
@@ -0,0 +1,24 @@
+function exportar(){
+    console.log('baixar')    
+    let linksUrl = JSON.stringify(getLocalStorage())
+    
+    const conteudoArquivo = linksUrl; // Conteúdo do arquivo
+    
+    const blob = new Blob([conteudoArquivo], {type: "text/plain"}); // Cria um objeto Blob com o conteúdo e o tipo do arquivo
+    
+    const urlArquivo = URL.createObjectURL(blob); // Cria uma URL para o objeto Blob
+    
+    const linkDownload = document.createElement("a"); // Cria um link para download
+    
+    linkDownload.href = urlArquivo; // Define a URL de download no link
+    
+    linkDownload.download = "links.txt"; // Define o nome do arquivo para download
+    
+    document.body.appendChild(linkDownload); // Adiciona o link ao corpo do documento
+    
+    linkDownload.click(); // Simula o clique no link para iniciar o download
+    
+    URL.revokeObjectURL(urlArquivo); // Remove a URL criada para o objeto Blob
+
+}
+    
---
+++ assets/js/pesquisar.js
@@ -0,0 +1,57 @@
+///////////////////////////
+//////// Google ///////////
+///////////////////////////
+
+const formGoogle = document.getElementById('search-form-google')
+const inputGoogle = document.getElementById('search-input-google')
+
+function buscarNoGoogle(){
+    if(inputGoogle.value.length > 0){
+        const query = inputGoogle.value;
+        const url = `https://www.google.com/search?q=${query}`;
+    
+        // Redirecione o usuário para a página de resultados do Google
+        window.location.href = url;
+    }
+}
+
+formGoogle.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buscarNoGoogle()
+})
+
+///////////////////////////
+//////// Youtube //////////
+///////////////////////////
+
+const formYoutube = document.getElementById('search-form-youtube');
+const inputYoutube = document.getElementById('search-input-youtube');
+
+function buscarNoYoutube(){
+    if(inputYoutube.value.length > 0){
+        const query = inputYoutube.value;
+    const url = `https://www.youtube.com/results?search_query=${query}`;
+
+    // Redirecione o usuário para a página de resultados do YouTube
+    window.location.href = url;
+    }
+}
+
+formYoutube.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buscarNoYoutube()
+});
+
+
+////---- função de evento 
+document.querySelectorAll('.label').forEach(l => {
+    l.addEventListener('click', (e)=>{
+        if(e.target.htmlFor == 'search-input-google'){
+            buscarNoGoogle()
+        }
+        if(e.target.htmlFor == 'search-input-youtube'){
+            buscarNoYoutube()
+        }
+    })
+})
+
